ace42f9387118f262742db73e69059ffd3043de0
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-18 10:27:14 +0530

    [Loveleen] ADD: Using annotations from lombok library

--- src/main/java/com/bridgelabz/employee_payroll/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employee_payroll/dto/EmployeeDTO.java
@@ -1,32 +1,38 @@
 package com.bridgelabz.employee_payroll.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class EmployeeDTO {
+
     private String name;
     private double salary;
 
-    public EmployeeDTO() {}
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+//    public EmployeeDTO() {}
+//    public EmployeeDTO(String name, double salary) {
+//        this.name = name;
+//        this.salary = salary;
+//    }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public String toString() {
-        return "name : " + name + " salary: " + salary;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public double getSalary() {
+//        return salary;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setSalary(double salary) {
+//        this.salary = salary;
+//    }
+//
+//    public String toString() {
+//        return "name : " + name + " salary: " + salary;
+//    }
 }
