020dd856b43dc7348724edebddadc8485a9f2da3
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-18 10:18:35 +0530

    [Loveleen] ADD: Using @Data and @AllArgsConstructor annotation

--- src/main/java/com/bridgelabz/employee_payroll/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employee_payroll/dto/ResponseDTO.java
@@ -1,31 +1,34 @@
 package com.bridgelabz.employee_payroll.dto;
 
-import org.springframework.http.HttpStatusCode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class ResponseDTO {
     private String message;
     private Object data;
     // private HttpStatusCode statusCode;
 
-    public ResponseDTO() {}
-    public ResponseDTO(String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
+//    public ResponseDTO() {}
+//    public ResponseDTO(String message, Object data) {
+//        this.message = message;
+//        this.data = data;
+//    }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public Object getData() {
+//        return data;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//
+//    public void setData(Object data) {
+//        this.data = data;
+//    }
 }
